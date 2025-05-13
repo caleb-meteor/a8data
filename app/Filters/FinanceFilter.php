33f1ca9a02f673ef6f5ea08760b38c2fcc70272c
caleb <caleb.meteor@gmail.com>
2025-05-13 21:51:28 +0800

    筛选过滤

--- app/Filters/FinanceFilter.php
+++ app/Filters/FinanceFilter.php
@@ -6,11 +6,6 @@ use Caleb\Practice\QueryFilter;
 
 class FinanceFilter extends QueryFilter
 {
-    public function teamId($teamId)
-    {
-        return $this->query->where('team_id', $teamId);
-    }
-
     public function productId($productId)
     {
         return $this->query->where('product_id', $productId);
